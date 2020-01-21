7697b42ce6aad383c73170cd9dc35d634866fa34
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-21 16:08:32 -0700

    Decided to have the Loader graphic be paused by default

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -10,6 +10,7 @@ const Loader = () => {
    
    const tl = gsap.timeline() //creating my timeline reference 
    useEffect(() => {
+      tl.pause() //start the animation in a paused state
       //animating the blue and yellow at the same time, to repeat forever
       tl.fromTo([blue.current, yellow.current], {y: 20}, {y: -20, yoyo: true, repeat: -1})
       //animating the red and green at the same time, to repeat forever
@@ -25,9 +26,9 @@ const Loader = () => {
             <circle ref={yellow} cx="94.3" cy="16.6" r="16.1" fill="#f4b61a" />
             <circle ref={green} cx="133.4" cy="16.6" r="16.1" fill="#009e52" />
          </svg>
-         {/* Added ability to pause and resume */}
+         {/* Added ability to play and pause */}
+         <button onClick={() => tl.play()}>Play</button>
          <button onClick={() => tl.pause()}>Pause</button>
-         <button onClick={() => tl.play()}>Resume</button>
       </Fragment>
    );
 }
