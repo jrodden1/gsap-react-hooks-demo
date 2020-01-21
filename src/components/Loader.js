49a7510d396aac5a7d70534a8d02d60f65ccbdea
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-21 16:03:43 -0700

    Added ability to pause and resume my loader graphic

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, Fragment } from 'react';
 import gsap from 'gsap'
 
 const Loader = () => {
@@ -8,8 +8,8 @@ const Loader = () => {
    const yellow = useRef(null);
    const green = useRef(null);
    
+   const tl = gsap.timeline() //creating my timeline reference 
    useEffect(() => {
-      const tl = gsap.timeline() //creating my timeline reference 
       //animating the blue and yellow at the same time, to repeat forever
       tl.fromTo([blue.current, yellow.current], {y: 20}, {y: -20, yoyo: true, repeat: -1})
       //animating the red and green at the same time, to repeat forever
@@ -17,13 +17,18 @@ const Loader = () => {
    })
 
    return (
-      <svg viewBox="0 0 150 33.2" width="180" height="150">
-         {/* Added References to my individual circle objects in my SVG */}
-         <circle ref={blue} cx="16.1" cy="16.6" r="16.1" fill="#527abd" />
-         <circle ref={red} cx="55.2" cy="16.6" r="16.1" fill="#de4431" />
-         <circle ref={yellow} cx="94.3" cy="16.6" r="16.1" fill="#f4b61a" />
-         <circle ref={green} cx="133.4" cy="16.6" r="16.1" fill="#009e52" />
-      </svg>
+      <Fragment>
+         <svg viewBox="0 0 150 33.2" width="180" height="150">
+            {/* Added References to my individual circle objects in my SVG */}
+            <circle ref={blue} cx="16.1" cy="16.6" r="16.1" fill="#527abd" />
+            <circle ref={red} cx="55.2" cy="16.6" r="16.1" fill="#de4431" />
+            <circle ref={yellow} cx="94.3" cy="16.6" r="16.1" fill="#f4b61a" />
+            <circle ref={green} cx="133.4" cy="16.6" r="16.1" fill="#009e52" />
+         </svg>
+         {/* Added ability to pause and resume */}
+         <button onClick={() => tl.pause()}>Pause</button>
+         <button onClick={() => tl.play()}>Resume</button>
+      </Fragment>
    );
 }
 
